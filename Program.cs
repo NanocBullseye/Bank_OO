4755facb6564041c95c49c91777cf922324a2f68
NanocBullseye <81162690+NanocBullseye@users.noreply.github.com>
2021-09-15 12:48:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            Client eduardo = new Client("Eduardo", 1234, 890, 333.33);
-            Client thulio = new Client("Thulio", 1234, 900, 3333333.33);
-            Client cana = new Client("Cana", 1234, 999, 3333333333333333333.33);
+            Client eduardo = new Client("Joao", 1234, 890, 333.33);
+            Client thulio = new Client("Jonas", 1234, 900, 3333333.33);
+            Client cana = new Client("Josias", 1234, 999, 3333333333333333333.33);
 
             Console.WriteLine("O titular da conta é: " + eduardo.titular);
             Console.WriteLine("O titular da conta é: " + thulio.titular);

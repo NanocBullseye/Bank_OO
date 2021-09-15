7806eb2ca81ff250d45a60d7c4df820576416f69
NanocBullseye <81162690+NanocBullseye@users.noreply.github.com>
2021-09-15 12:47:02 -0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,13 @@
+public class Client {
+    public string titular;
+    public int agencia;
+    public int conta;
+    public double saldo;
+
+    public Client(string titular, int agencia, int conta, double saldo) {
+        this.titular = titular;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = saldo;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client eduardo = new Client("Eduardo", 1234, 890, 333.33);
+            Client thulio = new Client("Thulio", 1234, 900, 3333333.33);
+            Client cana = new Client("Cana", 1234, 999, 3333333333333333333.33);
+
+            Console.WriteLine("O titular da conta é: " + eduardo.titular);
+            Console.WriteLine("O titular da conta é: " + thulio.titular);
+            Console.WriteLine("O titular da conte é: " + cana.titular);
+        }
+    }
+}
